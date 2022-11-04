02f1b3eeb715e1f1286281ff637e7416db6f8c9a
Ericfl4 <fernandezlunaeric@elpuig.xeill.net>
2022-11-04 16:39:32 +0100

    aa

--- src/lambdas/ExerciciL.java
+++ src/lambdas/ExerciciL.java
@@ -3,7 +3,7 @@ package lambdas;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-//pene
+
 public class ExerciciL {
 
     public static void main(String[] args) {
